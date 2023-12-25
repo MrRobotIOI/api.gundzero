d2d03a63bff4c2fb876f06a5a0e3b95b963a4779
Zed Ikejiani <zedikejiani@Zeds-MacBook-Pro.local>
2023-12-25 10:03:25 -0500

    I hate chrome

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ const sessionMiddleware = session({
   })
 })
 
-//app.enable('trust proxy');
+app.set('trust proxy', 'loopback')
 app.use(sessionMiddleware);
 const whitelist = ['http://localhost:5173', 'https://mrrobotioi.github.io',"https://gund-zero.onrender.com"];
 
